2f6bf9a8c66ec6bd1b2d015f2f4768b8f4a0558d
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-05 19:04:31 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,4 +6,4 @@ python -m scientists.runtime.cli 'research reproducibility' --json
 wget --no-check-certificate https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh 
 install numpy scipy scikit-learn pandas plotly matplotlib jupyter
 pip3 install jupyter
-
+pip3 install ipywidgets
