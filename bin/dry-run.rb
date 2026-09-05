058832a0e0add073bc1a3aa45c554e88268b6662
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-06 00:52:16 +0100

    Add dry-run script for dependency management

---
+++ bin/dry-run.rb
@@ -0,0 +1,7 @@
+$ bin/docker-dev-shell go_modules
+=> running docker development shell
+$ bin/dry-run.rb go_modules rsc/quote
+=> fetching dependency files
+=> parsing dependency files
+=> updating 2 dependencies
+...
